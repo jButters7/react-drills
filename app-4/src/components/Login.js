db15200a7761b20ecf46be7c903de8e24d1de4b2
Jake Butters <jacobbutters@gmail.com>
2020-08-19 16:10:08 -0600

    4th app complete

---
+++ app-4/src/components/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleUsernameChanges(input) {
+    this.setState({
+      username: input
+    })
+  }
+
+  handlePasswordChanges(input) {
+    this.setState({
+      password: input
+    })
+  }
+
+  displayAlert() {
+    alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <input placeholder="Username" onChange={(event) => this.handleUsernameChanges(event.target.value)} />
+        <input placeholder="Password" onChange={(event) => this.handlePasswordChanges(event.target.value)} />
+        <button onClick={() => this.displayAlert()}>Login</button>
+      </div>
+    )
+
+  }
+}
+export default Login;
